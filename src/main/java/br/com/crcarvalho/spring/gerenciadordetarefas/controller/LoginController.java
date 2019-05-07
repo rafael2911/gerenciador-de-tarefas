de3cce1e5ca27224f34548d0f5c2383a51459682
Rafael Carvalho <crcarvalho@outlook.com>
2019-05-07 12:14:44 -0300

    Impedindo que usuario logado acesse a página de login

--- src/main/java/br/com/crcarvalho/spring/gerenciadordetarefas/controller/LoginController.java
+++ src/main/java/br/com/crcarvalho/spring/gerenciadordetarefas/controller/LoginController.java
@@ -1,14 +1,22 @@
 package br.com.crcarvalho.spring.gerenciadordetarefas.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.crcarvalho.spring.gerenciadordetarefas.model.Usuario;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public ModelAndView formLogin() {
+	public ModelAndView formLogin(@AuthenticationPrincipal Usuario usuario) {
+		
+		if(usuario != null) {
+			return new ModelAndView("redirect:/tarefa/");
+		}
+		
 		return new ModelAndView("login/formLogin");
 	}
 	
